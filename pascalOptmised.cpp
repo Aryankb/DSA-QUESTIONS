47c27ad57238d13816b2e7bd86a4bb3702ceb5a1
Aryankb <118632488+Aryankb@users.noreply.github.com>
2023-04-30 20:45:46 +0530

    Update pascalOptmised.cpp

--- pascalOptmised.cpp
+++ pascalOptmised.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,prev,curr;
+    int n,prev;
     cout<<"Enter the no of rows :";
     cin>>n;
     for(int i=0;i<=n-1;i++){
         prev=1;
         cout<<prev<<" ";
-        for(float j=1;j<=i;j++){
-            curr=prev*((i-j+1)/(j));
-            cout<<curr<<" ";
-            prev=curr;
+        for(int j=1;j<=i;j++){
+            cout<<prev*(i-j+1)/j<<" ";
+            prev=prev*(i-j+1)/j;
         }
         cout<<endl;
     }
 }
 
-// took only j as float-space complexity
+// n need to take anything as float
 //no need to calculate factorials-time complexity
