8a91c1aa7feb22bd6e77cfd2900d55062dab391e
Aryankb <118632488+Aryankb@users.noreply.github.com>
2023-08-06 10:26:05 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -8,13 +8,16 @@ int main(){
         cin>>a[i];
     }
     for(int i=n;i>0;i--){
+        int f=0;
         for(int j=0;j<i-1;j++){
             if(a[j]>a[j+1]){
+                f=1;
                 int t=a[j];
                 a[j]=a[j+1];
                 a[j+1]=t;
             }
         }
+        if(f==0)break;
     }
     for(int i=0;i<n;i++){
         cout<<a[i];
